7ecd69bbc5acad4a871a541efa9136c8177b2057
Md Mobassher Hossain <mobassherpautex@gmail.com>
2025-03-08 14:52:18 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button className="bg-green-400 text-white">Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
